a63ced6c5972e3621b1f9a59eab9bd4407194a9e
Shaun Jackman <sjackman@gmail.com>
2016-12-18 11:05:35 -0800

    spearmint-arcs.py: Set parameter default values

--- spearmint-arcs.py
+++ spearmint-arcs.py
@@ -8,11 +8,11 @@ import subprocess
 
 # Run ABySS
 def main(job_id, params):
-    c = params['c']
-    e = params['e']
-    r = params['r']
-    a = params['a']
-    l = params['l']
+    c = params.get('c', 1)
+    e = params.get('e', 50000)
+    r = params.get('r', 0.05)
+    a = params.get('a', 1.0)
+    l = params.get('l', l)
     tsv_filename = "psitchensismt.psitchensis.bx.atleast4.c%d_e%d_r%f.arcs.a%f_l%d.links.scaffolds.stats.tsv" % (c, e, r, a, l)
     command = "make c=%d e=%d r=%f a=%f l=%d %s" % (c, e, r, a, l, tsv_filename)
     print command
@@ -28,4 +28,4 @@ def main(job_id, params):
     return -max_ng50
 
 if __name__ == "__main__":
-    main(None, {"c": 3, "e": 30000, "r": 0.05, "a": 0.9, "l": 2})
+    main(None, {})
